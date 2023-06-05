e6f41d41b37a75a49b7c80bc0a0c5cb5404a1773
Carlos G. MU <52215770+CarlosGabrielMoralesUmasi@users.noreply.github.com>
2023-06-05 12:47:15 -0500

    Update biseccion.m

--- analisis_numérico_functions/biseccion.m
+++ analisis_numérico_functions/biseccion.m
@@ -1,15 +1,16 @@
-function m=biseccion(f,a,b,err)
-  m=(a+b)/2;
-  c=0;
-  fprintf('|     k    |     a    |     b    |     m    |\n');  
-  while abs(b-a)>err
-    fprintf('|%10.6f|%10.6f|%10.6f|%10.6f|\n',c,a,b,m) 
-    if f(a)*f(m)<0;
-      b=m;
-    else 
-      a=m;
+function m = biseccion(f, a, b, err)
+  m = (a + b) / 2;
+  c = 0;
+  fprintf('|     k    |     a    |     b    |     m    |  f(a)   |  f(b)   |  f(m)   |  Error  |\n');
+  while abs(b - a) > err
+    fprintf('|%10d|%10.6f|%10.6f|%10.6f|%10.6f|%10.6f|%10.6f|%10.6f|\n', c, a, b, m, f(a), f(b), f(m), abs(b - a))
+    if f(a) * f(m) < 0
+      b = m;
+    else
+      a = m;
     endif
-    m=(a+b)/2;
-    c=c+1;
+    m = (a + b) / 2;
+    c = c + 1;
   endwhile
+  fprintf('|%10d|%10.6f|%10.6f|%10.6f|%10.6f|%10.6f|%10.6f|%10.6f|\n', c, a, b, m, f(a), f(b), f(m), abs(b - a))  # Imprimir la última iteración
 endfunction
